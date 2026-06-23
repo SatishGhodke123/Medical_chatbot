a2868824b15abe5ceb50316c0c1b523edcfb4953
SatishGhodke123 <satishghodke68@gmail.com>
2026-06-23 14:12:53 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical_chatbot",
+    version="0.1.0",
+    author="satish",
+    author_email="satish@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+)
